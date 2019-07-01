751a259a26c49be35e438d1e84ead7fe3961698f
yangjinlong <835383258@qq.com>
2019-07-02 05:31:45 +0800

    jx8

--- src/router.js
+++ src/router.js
@@ -4,8 +4,11 @@ import store from "./store";
 Vue.use(Router);
 
 const router = new Router({
-  mode: "history",
-  base: process.env.BASE_URL,
+  /*  mode: "history",
+  base: "/dist",*/
+  /*
+    base: process.env.BASE_URL,
+  */
   routes: [
     {
       path: "/",
